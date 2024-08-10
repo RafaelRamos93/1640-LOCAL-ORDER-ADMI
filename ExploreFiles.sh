7d31c3fc604db963e973047c2df622f1758c3803
RafaelRamos93 <rafael.ramosg93@gmail.com>
2024-08-09 18:47:24 -0600

    se modifica archivo ExploreFiles.sh

--- ExploreFiles.sh
+++ ExploreFiles.sh
@@ -1,10 +1,29 @@
-REPO_DIR="C/Citi/RepoPoC-Rafa/1640-LOCAL-ORDER-ADMI"
+#REPO_DIR="C/Citi/RepoPoC-Rafa/1640-LOCAL-ORDER-ADMI"
 
 # Cambia al directorio del repositorio
-cd "$REPO_DIR" || exit
+#cd "$REPO_DIR" || exit
 
-# Encuentra todos los archivos sin extensión en el repositorio
-for file in $(find . -type f ! -name "*.*"); do
+EXCLUDE_DIRS=(".git" ".vscode")
+EXCLUDE_FILES=(".gitignore")
+
+EXCLUDE_PATTERNS=("jenkins*" "Jenkins*")
+
+# Genera la lista de exclusión para el comando find
+
+EXCLUDE_PARAMS=""
+for dir in "${EXCLUDE_DIRS[@]}"; do
+    EXCLUDE_PARAMS+=" -path ./$dir -prune -o"
+done
+for file in "${EXCLUDE_FILES[@]}"; do
+    EXCLUDE_PARAMS+=" -name $file -o"
+done
+for pattern in "${EXCLUDE_PATTERNS[@]}"; do
+    EXCLUDE_PARAMS+=" -name '$pattern' -o"
+done
+
+
+# Encuentra todos los archivos sin extensión excluyendo las carpetas, archivos y patrones especificados
+eval "find . $EXCLUDE_PARAMS -type f ! -name '*.*' -print" | while read -r file; do
     # Verifica si el archivo es un archivo COBOL
     if file "$file" | grep -q 'COBOL'; then
         # Renombra el archivo para agregar la extensión .cbl
